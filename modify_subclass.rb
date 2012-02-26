3f37d51866678f0d118cf2db30b1bfae6a22cade
George Hemmings <gh@georgehemmings.com>
2012-02-26 18:06:15 +0000

    Add script to explore classes that modify their subclasses

---
+++ modify_subclass.rb
@@ -0,0 +1,33 @@
+class Automobile
+  attr_reader :features
+  
+  def initialize
+    @features = Hash.new(0)
+    yield self if block_given?
+  end
+  
+  def self.available_feature(*feature_names)
+    feature_names.each do |feature_name|
+      method_name = "add_#{feature_name}"
+      define_method(method_name) do |qty = 1|
+        add_feature(feature_name, qty)
+      end      
+    end
+  end
+  
+private  
+  
+  def add_feature(feature_name, qty = 1)
+    @features[feature_name] += qty
+  end
+end
+
+class Batmobile < Automobile  
+  available_feature :batphone, :bat_beam, :bat_ray, :mobile_bat_computer
+end
+
+custom_batmobile = Batmobile.new do |bm|
+  bm.add_batphone
+  bm.add_bat_beam 2
+  bm.add_bat_ray
+end
