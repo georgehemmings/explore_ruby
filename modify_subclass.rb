905e6ad57b5c46fe167ba8ca4f94f5d33917ede8
George Hemmings <gh@georgehemmings.com>
2012-02-26 18:50:08 +0000

    Replace qty parameter with options hash so method call is clearer

--- modify_subclass.rb
+++ modify_subclass.rb
@@ -9,15 +9,16 @@ class Automobile
   def self.available_feature(*feature_names)
     feature_names.each do |feature_name|
       method_name = "add_#{feature_name}"
-      define_method(method_name) do |qty = 1|
-        add_feature(feature_name, qty)
+      define_method(method_name) do |options = { qty: 1 }|
+        add_feature(feature_name, options)
       end      
     end
   end
   
 private  
   
-  def add_feature(feature_name, qty = 1)
+  def add_feature(feature_name, options = {})
+    qty = options[:qty] || 1
     @features[feature_name] += qty
   end
 end
@@ -28,6 +29,6 @@ end
 
 custom_batmobile = Batmobile.new do |bm|
   bm.add_batphone
-  bm.add_bat_beam 2
+  bm.add_bat_beam(qty: 2)
   bm.add_bat_ray
 end
