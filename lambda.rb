3babd567c8a6db0c99f0d1181c6dc22a4dcbf68c
George Hemmings <gh@georgehemmings.com>
2012-03-02 17:58:19 +0000

    Add script to explore lambdas

---
+++ lambda.rb
@@ -0,0 +1,16 @@
+require "bigdecimal"
+
+class String
+
+  def convert(convertor)
+    convertor.call self
+  end
+end
+
+from_inches_to_cms = lambda do |inches_string|
+  conversion_factor = BigDecimal.new("2.25")
+  cms = BigDecimal.new(inches_string) * conversion_factor
+  "%.2f" % cms
+end
+
+"100".convert from_inches_to_cms  # => "225.00"
